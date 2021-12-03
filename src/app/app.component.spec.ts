694cbe3954c1b2fb37465769c526f5e07f88d914
Thomas Czogalik <thomas.czogalik@andrena.de>
2021-12-03 14:57:40 +0100

    add testing-library test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,54 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {render, screen} from '@testing-library/angular';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  describe('without testing-library', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [
+          AppComponent
+        ],
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+        schemas: [
+          CUSTOM_ELEMENTS_SCHEMA
+        ]
+      }).compileComponents();
+    });
 
-  it(`should have as title 'testing-library-angular-try'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-library-angular-try');
-  });
+    it('should create the app', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing-library-angular-try app is running!');
+    it(`should have as title 'testing-library-angular-try'`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('testing-library-angular-try');
+    });
+
+    it('should render title', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.content span')?.textContent).toContain('testing-library-angular-try app is running!');
+    });
+  });
+  describe('with testing-library', () => {
+    beforeEach(async () => {
+      await render(AppComponent, {
+        schemas: [
+          CUSTOM_ELEMENTS_SCHEMA
+        ]
+      });
+    });
+
+    it('should render title', () => {
+      expect(screen.getByText('testing-library-angular-try app is running!'));
+
+      expect(screen.getByRole('main')).toHaveTextContent('testing-library-angular-try app is running!');
+    });
   });
 });
